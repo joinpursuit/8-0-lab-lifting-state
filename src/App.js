dc63878f15771d136573363dc45a0bc26716221a
Kylie-Lang1 <kylielang@pursuit.org>
2022-10-14 00:51:11 -0400

    Updated Attendees.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import eventsData from "./data";
 // import Attendees from "./Attendees";
-// import Event from "./Components/Event";
+import Event from "./Components/Event";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import NewEventForm from "./Components/NewEventForm";
@@ -9,28 +9,9 @@ import NewEventForm from "./Components/NewEventForm";
 function App() {
 
   const [events, setEvents] = useState(eventsData);
-  const [showAttendees, setShowAttendees] = useState(false);
-
+ 
   function handleAddEvent(event) {
     setEvents([event, ...events]);
-  }
-
-  function toggleEventAttendees() {
-    setShowAttendees(!showAttendees);
-  }
-
-  function updateEventAttendance(eventId, attendeeId) {
-    const eventArray = [...events];
-    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
-    const event = { ...eventArray[eventIndex] };
-    const personIndex = event.people.findIndex(
-      (person) => person.id === attendeeId
-    );
-    const peopleArray = [...event.people];
-    peopleArray[personIndex].attendance = !peopleArray[personIndex].attendance;
-    event.people = peopleArray;
-    eventArray[eventIndex] = event;
-    setEvents(eventArray);
   }
 
   return (
@@ -43,10 +24,9 @@ function App() {
         <div className="events">
           <ul>
             {events.map((event) => {
-              const { people: attendees } = event;
-
+              
               return (
-                
+                <Event />
               );
             })}
           </ul>
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,6 +1,29 @@
 import { useState } from "react";
+import Attendees from "./Attendees";
 
 export default function Event() {
+
+  const { people: attendees } = event;
+
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  function updateEventAttendance(eventId, attendeeId) {
+    const eventArray = [...events];
+    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
+    const event = { ...eventArray[eventIndex] };
+    const personIndex = event.people.findIndex(
+      (person) => person.id === attendeeId
+    );
+    const peopleArray = [...event.people];
+    peopleArray[personIndex].attendance = !peopleArray[personIndex].attendance;
+    event.people = peopleArray;
+    eventArray[eventIndex] = event;
+    setEvents(eventArray);
+  }
 
   return(
     <>
@@ -12,53 +35,7 @@ export default function Event() {
         <br />
         <span>Organized by: {event.organizer} </span>
         <br />
-        <>
-          <button onClick={toggleEventAttendees}>
-            {!showAttendees ? "Show Attendees" : "Hide Attendees"}
-          </button>
-
-          {showAttendees ? (
-            <div className="attendees">
-              {attendees.map((attendee, index) => (
-                <>
-                  <div key={attendee.id} className="attendee">
-                    <p>
-                      <img
-                        src={attendee.avatar}
-                        alt={attendee.firstName}
-                      />
-                      {"   "}
-                      <span>
-                        {" "}
-                        {attendee.firstName} {attendee.lastName}{" "}
-                      </span>
-                    </p>
-                    <p>
-                      <button
-                        className="clickable"
-                        onClick={() =>
-                          updateEventAttendance(
-                            event.id,
-                            attendee.id
-                          )
-                        }
-                      >
-                        Attending:
-                      </button>
-                      <span>
-                        {attendee.attendance ? "✅" : "❌"}
-                      </span>
-                    </p>
-
-                    <p>
-                      <span>Note:</span> {attendee.note}
-                    </p>
-                  </div>
-                </>
-              ))}
-            </div>
-          ) : null}
-        </>
+        <Attendees />
       </li>
     </>
 
