fac4e1cf74c5e37a344063284c2cb28a4412f481
minagrullon <minagrullon@pursuit.org>
2022-10-15 19:39:04 -0400

    passes cypress

--- src/Components/Attendee.js
+++ src/Components/Attendee.js
@@ -1,3 +1,28 @@
-export default function Attendee() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Attendee({ updateEventAttendance, event, attendee }) {
+  return (
+    <>
+      <div key={attendee.id} className="attendee">
+        <p>
+          <img src={attendee.avatar} alt={attendee.firstName} />
+          <span>
+            {attendee.firstName} {attendee.lastName}
+          </span>
+        </p>
+        <p>
+          <button
+            className="clickable"
+            onClick={() => updateEventAttendance(event.id, attendee.id)}
+          >
+            Attending:
+          </button>
+          <span>{attendee.attendance ? "✅" : "❌"}</span>
+        </p>
+        <p>
+          <span>Note:</span> {attendee.note}
+        </p>
+      </div>
+    </>
+  );
 }
