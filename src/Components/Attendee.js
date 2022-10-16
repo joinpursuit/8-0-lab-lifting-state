b1add42dc644b24615fe5c2db0246b55524f0a02
Mendoza Holger <mendozadavidh111@gmail.com>
2022-10-16 13:01:14 -0400

    lab done

--- src/Components/Attendee.js
+++ src/Components/Attendee.js
@@ -1,3 +1,14 @@
-export default function Attendee() {
-  return;
+export default function Attendee({
+  eventID,
+  eventToShow,
+  toggleEventAttendees,
+  showAttendees,
+}) {
+  return (
+    <button onClick={() => toggleEventAttendees(eventID)}>
+      {showAttendees && eventToShow == eventID
+        ? "Hide Attendees"
+        : "Show Attendees"}
+    </button>
+  );
 }
--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,56 @@
-export default function Attendees() {
-  return;
+export default function Attendees({
+  events,
+  setEvents,
+  eventID,
+  eventToShow,
+  attendees,
+  showAttendees,
+}) {
+  function updateEventAttendance(eventId, attendeeId) {
+    const eventArray = [...events];
+    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
+    const event = { ...eventArray[eventIndex] };
+    const personIndex = event.people.findIndex(
+      (person) => person.id === attendeeId
+    );
+    const peopleArray = [...event.people];
+    peopleArray[personIndex].attendance = !peopleArray[personIndex].attendance;
+    event.people = peopleArray;
+    eventArray[eventIndex] = event;
+    setEvents(eventArray);
+  }
+
+  return (
+    <>
+      {showAttendees && eventToShow == eventID ? (
+        <div className="attendees">
+          {attendees.map((attendee, index) => (
+            <div key={attendee.id} className="attendee">
+              <p>
+                <img src={attendee.avatar} alt={attendee.firstName} />
+                {"   "}
+                <span>
+                  {" "}
+                  {attendee.firstName} {attendee.lastName}{" "}
+                </span>
+              </p>
+              <p>
+                <button
+                  className="clickable"
+                  onClick={() => updateEventAttendance(eventID, attendee.id)}
+                >
+                  Attending:
+                </button>
+                <span>{attendee.attendance ? "✅" : "❌"}</span>
+              </p>
+
+              <p>
+                <span>Note:</span> {attendee.note}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
