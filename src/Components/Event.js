abc06b81880e4a1b48457455f33aef51ef0d2ea9
Lennie Nurse <lnursej@gmail.com>
2022-10-16 22:08:59 -0400

    "update"

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,40 @@
-export default function Event() {
-  return;
+import { useState } from "react"
+import Attendees from "./Attendees"
+
+export default function Event({ event, updateEventAttendance, attendees }) {
+  const [showAttendees, setShowAttendees] = useState(false)
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees)
+  }
+
+  return (
+    <div className="event">
+      <>
+        <li key={event.id}>
+          <img src={event.eventImage} alt={event.name} />
+          <h5>
+            {event.name} {event.eventType}
+          </h5>
+          <br />
+          <span>Organized by: {event.organizer} </span>
+          <br />
+
+          <>
+            <button onClick={toggleEventAttendees}>
+              {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+            </button>
+
+            {showAttendees ? (
+              <Attendees
+                attendees={attendees}
+                event={event}
+                updateEventAttendance={updateEventAttendance}
+              />
+            ) : null}
+          </>
+        </li>
+      </>
+    </div>
+  )
 }
