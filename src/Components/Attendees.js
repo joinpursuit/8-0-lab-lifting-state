6e77faa8da2946a160bd455b8309fc3dc73e1427
MarsiaRochelin <marsrochelin@pursuit.org>
2022-10-16 23:28:17 -0400

    progress

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,25 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({
+  showAttendees,
+  attendees,
+  updateEventAttendance,
+}) {
+  return (
+    <>
+      {showAttendees ? (
+        <div className="attendees">
+          {attendees.map((attendee, index) => (
+            <>
+              <Attendee
+                attendee={attendee}
+                updateEventAttendance={updateEventAttendance}
+                index={index}
+              />
+            </>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,35 @@
-export default function Event() {
-  return;
+import Attendee from "./Attendee";
+import Attendees from "./Attendees";
+
+export default function Event({showAttendees, events,toggleEventAttendees, updateEventAttendance}) {
+  return (
+    <div className="events">
+      <ul>
+        {events.map((event) => {
+          const { people: attendees } = event;
+
+          return (
+            <>
+              <li key={event.id}>
+                <img src={event.eventImage} alt={event.name} />
+                <h5>
+                  {event.name} {event.eventType}
+                </h5>
+                <br />
+                <span>Organized by: {event.organizer} </span>
+                <br />
+                <>
+                  <button onClick={toggleEventAttendees}>
+                    {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                  </button>
+                      <Attendees attendees={attendees}/>
+                 
+                </>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
