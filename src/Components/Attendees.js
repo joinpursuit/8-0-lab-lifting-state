1d7d23face8b474018662759176f7375e659187e
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-17 06:14:35 -0400

    tests pass

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,15 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({ event, attendees, updateEventAttendance }) {
+  return (
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+        <Attendee
+          event={event}
+          attendee={attendee}
+          updateEventAttendance={updateEventAttendance}
+        />
+      ))}
+    </div>
+  );
 }
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,34 @@
-export default function Event() {
-  return;
+import { useState } from "react";
+import Attendees from "./Attendees";
+
+export default function Event({ event, attendees, updateEventAttendance }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  return (
+    
+    <li className="event" key={event.id}>
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <>
+        <button onClick={toggleEventAttendees}>
+          {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+        </button>
+
+        {showAttendees ? (
+          <Attendees
+            event={event}
+            attendees={attendees}
+            updateEventAttendance={updateEventAttendance}
+          />
+        ) : null}
+      </>
+    </li>
+  );
 }
