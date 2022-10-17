a87a6386f6c5ab08f0c3f7be72972ff505ba7025
Lennie Nurse <lnursej@gmail.com>
2022-10-16 22:48:44 -0400

    fixing issues with merging problem

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function Header() {
   return (
     <>
@@ -6,12 +5,5 @@ export default function Header() {
         <h1 className="color-change-5x">RSVP App</h1>
       </header>
     </>
-=======
-export default function Header({ Header }) {
-  return (
-    <header>
-      <h1 className="color-change-5x">RSVP App</h1>
-    </header>
->>>>>>> 2dbedeb56b96444669e1a1c753de9215516f4607
   )
 }
