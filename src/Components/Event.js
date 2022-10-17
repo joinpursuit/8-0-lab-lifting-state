ee3bcc1a4115ab0ede022c798d7c962abff7a078
bowersjuan <bowersjuan@gmail.com>
2022-10-16 21:59:18 -0400

    Move Attendees component into Attendees.js and pass props into it

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,5 @@
+import Attendees from "./Attendees";
+
 export default function Event({
   event,
   toggleEventAttendees,
@@ -15,45 +17,13 @@ export default function Event({
         <br />
         <span>Organized by: {event.organizer} </span>
         <br />
-        <>
-          <button onClick={toggleEventAttendees}>
-            {!showAttendees ? "Show Attendees" : "Hide Attendees"}
-          </button>
-
-          {showAttendees ? (
-            <div className="attendees">
-              {attendees.map((attendee, index) => (
-                <>
-                  <div key={attendee.id} className="attendee">
-                    <p>
-                      <img src={attendee.avatar} alt={attendee.firstName} />
-                      {"   "}
-                      <span>
-                        {" "}
-                        {attendee.firstName} {attendee.lastName}{" "}
-                      </span>
-                    </p>
-                    <p>
-                      <button
-                        className="clickable"
-                        onClick={() =>
-                          updateEventAttendance(event.id, attendee.id)
-                        }
-                      >
-                        Attending:
-                      </button>
-                      <span>{attendee.attendance ? "✅" : "❌"}</span>
-                    </p>
-
-                    <p>
-                      <span>Note:</span> {attendee.note}
-                    </p>
-                  </div>
-                </>
-              ))}
-            </div>
-          ) : null}
-        </>
+        <Attendees
+          toggleEventAttendees={toggleEventAttendees}
+          showAttendees={showAttendees}
+          attendees={attendees}
+          updateEventAttendance={updateEventAttendance}
+          event={event}
+        />
       </li>
     </>
   );
