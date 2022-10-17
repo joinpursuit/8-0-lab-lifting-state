6ac16f3ab3a685a712cbe7f2f3685637e8a56635
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-17 02:12:23 -0400

    lifting lab

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,20 @@
-export default function Attendees() {
-  return;
-}
+import Attendee from "./Attendee"
+
+export default function Attendees({attendees, updateEventAttendance, event}) {
+  return(
+
+      <div className="attendees">
+        {attendees.map((attendee, index) => (
+          <>
+            <Attendee 
+            index={index}
+            attendee={attendee} 
+            updateEventAttendance={updateEventAttendance} 
+            event={event}
+            />
+          </>
+        ))}
+      </div>
+    )
+  }
+  
