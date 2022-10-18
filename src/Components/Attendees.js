ceccbe90191199d0f6cc9c2c75ad2bbc5980ccc7
Fadila-Ali <fadilaali@pursuit.org>
2022-10-18 10:39:58 -0400

    all done

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,5 +1,5 @@
 import Attendee from "./Attendee";
-export default function Attendees(updateEventAttendance, attendees, event) {
+export default function Attendees({ updateEventAttendance, attendees, event }) {
   return (
     <div className="attendees">
       {attendees.map((attendee, index) => (
