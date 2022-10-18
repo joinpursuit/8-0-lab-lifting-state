b2b74f7cecfe4d215ac62325fda34e788d77e01c
cmm-7 <carlosmendoza@pursuit.org>
2022-10-18 01:15:33 -0400

    lab-lifting-state complete

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,23 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({event, attendees, toggleEventAttendees, showAttendees, updateEventAttendance }) {
+  return (
+    <li className="event" key={event.id}>
+                    <img src={event.eventImage} alt={event.name} />
+                    <h5>
+                      {event.name} {event.eventType}
+                    </h5>
+                    <br />
+                    <span>Organized by: {event.organizer} </span>
+                    <br />
+                    <>
+                      <Attendees 
+                        event={event} 
+                        attendees={attendees} 
+                        toggleEventAttendees={toggleEventAttendees} 
+                        updateEventAttendance={updateEventAttendance}
+                      />
+                    </>
+                  </li>
+  );
 }
