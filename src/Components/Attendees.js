2da211d388029ff218607ece8a054d69ece9afc2
SharukhGV <sharukhvelupillai@pursuit.org>
2022-10-19 17:58:43 -0400

    Almost Finished

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,24 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({ event, attendees, updateEventAttendance,toggleEventAttendees,showAttendees}) {
+  // const [showAttendees, setShowAttendees] = useState(false);
+  
+  return(
+    
+    <div>
+      <button onClick={toggleEventAttendees}>
+                  {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                </button>
+
+                {showAttendees ? (
+                  <div className="attendees">
+                    {attendees.map((attendee, index) => (
+                      <>
+                        < Attendee index={index}  updateEventAttendance={updateEventAttendance} event ={event} attendee={attendee} />
+                      </>
+                    ))}
+                  </div>
+                ) : null}
+    </div>
+  )
 }
