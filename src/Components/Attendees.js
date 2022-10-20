cf5b331310b8bd73dd6234aadac24d6883810c30
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-10-19 20:10:36 -0400

    Add state and move html to appropriate components. Pass all cypress tests.

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,13 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({attendees , updateEventAttendance, event}) {
+  return (
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+        <>
+          <Attendee attendee={attendee} updateEventAttendance={updateEventAttendance} event={event}/>
+        </>
+      ))}
+    </div>
+  );
 }
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,39 @@
-export default function Event() {
-  return;
+import { useState } from "react";
+import Attendees from "./Attendees";
+
+export default function Event({
+  attendees,
+  event,
+  updateEventAttendance,
+}) {
+  const [showAttendees, setShowAttendees] = useState(false);
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  return (
+    <>
+      <li key={event.id} className="event">
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <>
+          <button onClick={toggleEventAttendees}>
+            {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+          </button>
+
+          {showAttendees ? (
+            <Attendees
+              attendees={attendees}
+              updateEventAttendance={updateEventAttendance}
+              event={event}
+            />
+          ) : null}
+        </>
+      </li>
+    </>
+  );
 }
