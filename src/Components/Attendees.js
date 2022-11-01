e4fa46134a05d5476e94eb38f4022590dbe9d1eb
melanieinpursuit <melaniegonzalez@pursuit.org>
2022-11-01 02:14:04 -0400

    Finished lab.

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,15 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+
+export default function Attendees({ attendees, event, updateEventAttendance }) {
+  return (
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+        <Attendee
+          updateEventAttendance={updateEventAttendance}
+          attendee={attendee}
+          event={event}
+        />
+      ))}
+    </div>
+  );
 }
