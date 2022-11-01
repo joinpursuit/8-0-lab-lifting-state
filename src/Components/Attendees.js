7b4fca6deed4934d5ca9981e3771ea5a997ebfbb
DanielR2105 <rodriguez.daniel2105@gmail.com>
2022-10-31 23:46:04 -0400

    finished lab

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,11 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee"
+
+export default function Attendees({updateEventAttendance, attendees, event}) {
+  return (
+    <div className="attendees">
+              {attendees.map((attendee) => (
+                <Attendee attendee={attendee} event={event} updateEventAttendance={updateEventAttendance} />
+              ))}
+            </div>
+  )
 }
