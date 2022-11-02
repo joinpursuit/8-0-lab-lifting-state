c53194887f28e5e7f741b1c53748686517aaa787
Karyn Huston <huston.karyn@gmail.com>
2022-11-02 13:31:38 -0400

    karynH

--- src/Components/Attendee.js
+++ src/Components/Attendee.js
@@ -1,3 +1,35 @@
-export default function Attendee() {
-  return;
-}
+import { useState } from "react";
+export default function Attendee({ attendee, event, updateEventAttendance }) {
+ 
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  return (
+    <div key={attendee.id} className="attendee">
+    <p>
+      <img src={attendee.avatar} alt={attendee.firstName} />
+      {"   "}
+      <span>
+        {" "}
+        {attendee.firstName} {attendee.lastName}{" "}
+      </span>
+    </p>
+    <p>
+      <button
+        className="clickable"
+        onClick={() => updateEventAttendance(event.id, attendee.id)}
+      >
+        Attending:
+      </button>
+      <span>{attendee.attendance ? "✅" : "❌"}</span>
+    </p>
+
+    <p>
+      <span>Note:</span> {attendee.note}
+    </p>
+  </div>
+  );
+  }
