9d582c26703b2ee4148b2f75bd2802d291cd19e1
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2022-11-08 08:17:33 -0500

    done

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,12 @@
-export default function Attendees() {
-  return;
+
+import Attendee from "./Attendee";
+
+export default function Attendees({ attendees, event, updateEventAttendance }) {
+  
+  return (
+    <div className="attendees">
+       {/* attendeesList.map(attendee => <Attendee key={} data={} />) */}
+      {attendees.map((attendee) => <Attendee key={attendee.id} attendee={attendee} updateEventAttendance={updateEventAttendance} event={event} />)}
+    </div>
+  )
 }
