f31370c159f5016635fe70403efe09937cc1f7db
bigdaddyjean <jeannappoleon@pursuit.org>
2022-11-16 07:31:58 -0500

    finished

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,16 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({ event, updateEventAttendance }) {
+  return (
+    <li className="event" key={event.id}>
+      <img src={event.eventImage} alt={event.name} />
+      <h5>
+        {event.name} {event.eventType}
+      </h5>
+      <br />
+      <span>Organized by: {event.organizer} </span>
+      <br />
+      <Attendees event={event} updateEventAttendance={updateEventAttendance} />
+    </li>
+  );
 }
