fc2f51b720d3ccb86ae0fdfff1d0c3fb2185a9f0
dilrubamajumder <dilrubamajumder@pursuit.org>
2022-12-04 20:46:11 -0500

    Done and dusted

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,37 @@
-export default function Event() {
-  return;
+import React,{useState} from "react";
+import Attendees from "./Attendees";
+
+export default function Event({event, updateEventAttendance}) {
+
+  const [showAttendees, setShowAttendees] = useState(false);
+  const { people: attendees } = event;
+
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+  return(
+    <li className="event">
+    <img src={event.eventImage} alt={event.name} />
+    <h5>
+      {event.name} {event.eventType}
+    </h5>
+    <br />
+    <span>Organized by: {event.organizer} </span>
+    <br />
+    <>
+      <button onClick={toggleEventAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+
+      {showAttendees ? (
+      <Attendees 
+      event={event}
+      attendees={attendees}
+      updateEventAttendance={updateEventAttendance}
+      />
+      ) : null}
+    </>
+  </li>
+  );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,8 @@
+
 export default function Header() {
-  return;
+  return (
+    <header>
+    <h1 className="color-change-5x">RSVP App</h1>
+  </header>
+  );
 }
