7e8ad9058bf1eac855f481a90214e368b4ab650d
jasn4real <tobeobi@pursuit.org>
2022-12-11 00:35:28 -0500

    Lifting State Done

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,33 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+import { useState } from "react";
+
+export default function Event({ attendees, event, updateEventAttendance }) {
+
+const [showAttendees, setShowAttendees] = useState(false);
+
+function toggleEventAttendees() {
+  setShowAttendees(!showAttendees);
+}
+
+  return (
+    <div className="event">
+      <li key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>{event.name} {event.eventType}</h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <>
+          <button onClick={toggleEventAttendees}>
+            {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+          </button>
+            {showAttendees ? (
+            <Attendees 
+              attendees={attendees}
+              updateEventAttendance={updateEventAttendance}
+              event={event} />) : null}
+          </>
+      </li>
+    </div>
+  );
 }
